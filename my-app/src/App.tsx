7f2629abc98b308b02acdbca4df1080065efa768
Mark Atherton <markatherton@talkholiday.com>
2019-12-16 17:58:23 +0000

    changed App.ts to class component and added a state counter

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,18 +6,39 @@ type AppProps = {
   yourname?: string
 }
 
-const App: React.FC<AppProps> = ({yourname = "yourname"}) => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello {yourname}!
-        </p>
-        
-      </header>
-    </div>
-  );
+type AppState = {
+  count: number
+}
+
+class App extends React.Component<AppProps, AppState>{
+
+  state: AppState = {
+    count: 0
+  }
+
+  handleMinus = () => {
+    this.setState({count: this.state.count - 1})
+  }
+
+  handlePlus = () => {
+    this.setState( {count: this.state.count + 1} )
+  }
+
+  render() { 
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello {this.props.yourname}!
+          </p>
+          <button onClick={this.handlePlus }>+</button>
+          {this.state.count}
+          <button onClick={this.handleMinus }>-</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
